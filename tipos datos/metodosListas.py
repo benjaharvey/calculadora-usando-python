fbeaedbdd560f60195c3efedcf9ddb74c15975e5
Benjamin Harvey <benjaharvey@gmail.com>
2025-01-22 17:20:14 -0300

    metodos para operar listas parte 1

---
+++ tipos datos/metodosListas.py
@@ -0,0 +1,17 @@
+# creando una lista con list()
+lista = list(["hola", "benja", 34])
+print(lista)
+
+#devuelve cantidad de elementos en la lista
+resultado = len(lista)
+print(resultado)
+
+#agrega un elemento al final de la lista
+lista.append("chadbenja")
+
+print(lista)
+
+lista.insert(1, "rayven sos horrible")
+
+print(lista)
+
