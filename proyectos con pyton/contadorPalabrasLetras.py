4822a3245b3554a1fbf198e46c42ed6e10e63094
Benjamin Harvey <benjaharvey@gmail.com>
2025-01-22 01:17:19 -0300

    Cree un programa que cuenta letras y palabras en python

---
+++ proyectos con pyton/contadorPalabrasLetras.py
@@ -0,0 +1,12 @@
+# Escribe un programa que reciba un texto como entrada y cuente la cantidad de palabras o letras.
+
+palabra = input("Ingrese una palabra: ")
+
+print("La cantidad de letras de la palabra ingresada es: " + str(len(palabra)))
+
+
+palabras = input("Ingrese una frase: ")
+
+print("La cantidad de palabras de la frase ingresada es: " + str(len(palabras.split())))
+
+# Estos proyectos estan siendo mas sencillo de lo que me esperaba me parece que tengo que empezar a hacer cosas un poco mas grandes :)
