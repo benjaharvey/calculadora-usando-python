17b82c1337211ac51877924298c1b9aa4f2b9b9a
Benjamin Harvey <benjaharvey@gmail.com>
2025-01-29 15:06:59 -0300

    Agenda de contactos en python, primera funcion hecha

---
+++ proyectos con pyton/sistemaContactos.py
@@ -0,0 +1,26 @@
+# Sistema de contactos utilizando archivos de texto en Python :D
+# Utilize un dataclass ya que, segun lo que encontre en internet, es lo mas parecido a un typedef struct de C, que es lenguaje al cual estoy mas acostumbrado a programar
+from dataclasses import dataclass
+
+  
+@dataclass
+class contacto:
+    nombreApellido: str
+    email: str
+    numTelefono: str
+
+
+def agregarContacto():
+  with open("archivoContactos.txt", "a") as archivoContactos:
+    nombreApellido = input("Ingrese el nombre y apellido del contacto: ")
+    email = input("Ingrese el email: ")
+    numTelefono = input("Ingrese el numero de telefono: ")
+    
+    nuevoContacto = contacto(nombreApellido, email, numTelefono)
+    
+    archivoContactos.write(f"{nuevoContacto.nombreApellido}, {nuevoContacto.email}, {nuevoContacto.numTelefono}\n")
+    
+    print("Contacto añadido exitosamente!\n")
+    
+agregarContacto()
+
