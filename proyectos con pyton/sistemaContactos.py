5ee87ea7ddc9a4952d948d8906af15cbb56d5465
Benjamin Harvey <benjaharvey@gmail.com>
2025-01-30 20:11:35 -0300

    sistema de contactos en python casi listo, solo falta comentar y añadir una funcion que permita eliminar contactos

--- proyectos con pyton/sistemaContactos.py
+++ proyectos con pyton/sistemaContactos.py
@@ -9,23 +9,58 @@ class contacto:
     email: str
     numTelefono: str
 
-
 def agregarContacto():
-  with open("archivoContactos.txt", "a") as archivoContactos:
-    nombreApellido = input("Ingrese el nombre y apellido del contacto: ")
-    email = input("Ingrese el email: ")
-    numTelefono = input("Ingrese el numero de telefono: ")
+  with open("archivoContactos.txt", "a") as archivoContacto:
+    nuevoNombre = input("Ingrese el nombre y apellido del contacto que desea ingresar: ")
+    nuevoEmail = input("Ingrese el email del contacto que desea agregar: ")
+    nuevoTelefono = input("Ingrese el telefono del nuevo contacto: ")
     
-    nuevoContacto = contacto(nombreApellido, email, numTelefono)
+    nuevoContacto = contacto(nuevoNombre, nuevoEmail, nuevoTelefono)
     
-    archivoContactos.write(f"{nuevoContacto.nombreApellido}, {nuevoContacto.email}, {nuevoContacto.numTelefono}\n")
+    archivoContacto.write(f"{nuevoContacto.nombreApellido}, {nuevoContacto.email}, {nuevoContacto.numTelefono}\n")
     
-    print("Contacto añadido exitosamente!\n")
-    
-    
-    
-def mostrarContactos():
-  with open("archivoContactos.txt", "r") as archivoContactos:
-    for linea in archivoContactos:
-      print(linea)
+    print("Contacto almacenado de forma exitosa")
+ 
 
+ 
+def buscarContacto():
+  with open("archivoContactos.txt", "r") as archivoContacto:
+    busqueda = input("Ingrese el numero de telefono del contacto que desee buscar: ")
+    encontrado = False
+    for linea in archivoContacto:
+      
+      if busqueda in linea:
+        print(f"Contacto encontrado!, {linea.strip()}")
+        encontrado = True
+        break
+    
+    if encontrado == False:
+      print("No existe ese contacto!")
+
+
+
+def visualizarTodos():
+  with open("archivosContactos.txt", "r") as archivoContacto:
+    
+    try:
+     print("Los contactos que estan en el sistema son los siguientes: ")
+     for lineas in archivoContacto:
+      print(lineas)
+    
+    except FileNotFoundError:
+      print("El archivo no existe!, crealo usando la opcion 1")
+
+def menu():
+  opcion = int(input("1- ingresar contacto; 2- buscar contacto; 3- visualizar todos los contactos; 4- eliminar contacto especifico; 5- eliminar todos: "))
+  
+  if opcion == 1:
+    agregarContacto()
+  
+  if opcion == 2:
+    buscarContacto()
+  
+  if opcion == 3:
+    visualizarTodos()    
+
+
+menu()
